7febe5b298177507f02399b4da46d983ea8738a2
lais santos <laissdj49@gmail.com>
2024-08-22 21:06:39 +0200

    update layout

--- app/src/main/java/br/com/stonesdk/sdkdemo/activities/manageStoneCode/ManageStoneCodeLayout.kt
+++ app/src/main/java/br/com/stonesdk/sdkdemo/activities/manageStoneCode/ManageStoneCodeLayout.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -36,7 +37,7 @@ fun ManageStoneCodeContent(
 ) {
     Column(
         modifier = Modifier
-            .padding(horizontal = 16.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
             .fillMaxSize()
     ) {
         OutlinedTextField(
@@ -51,17 +52,18 @@ fun ManageStoneCodeContent(
         )
         Spacer(modifier = Modifier.height(8.dp))
         Button(
+            modifier = Modifier.align(Alignment.End),
             onClick = {
                 onEvent(ManageStoneCodeEvent.ActivateStoneCode)
             },
             enabled = model.stoneCodesActivated.isNotEmpty(),
             content = {
-                Text(text = "Ativar")
+                Text(text = "Ativar Stone Code")
             }
         )
 
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "StoneCodes ativados")
+        Text(text = "Stone codes ativados")
         LazyColumn(modifier = Modifier.weight(1f)) {
             itemsIndexed(
                 items = model.stoneCodesActivated,

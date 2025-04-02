6e09b2a06accbb4f79367124db0ed81db6d8fc77
João Carlos Ferreira Marques <joaocarlusferrera@gmail.com>
2025-04-02 11:36:08 -0300

    removing click listener for send email because of dependency on internal classes;

--- app/src/main/java/br/com/stonesdk/sdkdemo/activities/transaction/list/TransactionListViewModel.kt
+++ app/src/main/java/br/com/stonesdk/sdkdemo/activities/transaction/list/TransactionListViewModel.kt
@@ -1,33 +1,24 @@
 package br.com.stonesdk.sdkdemo.activities.transaction.list
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.stone.sdk.android.error.StoneStatus
 import br.com.stonesdk.sdkdemo.utils.parseCentsToCurrency
-import co.stone.posmobile.datacontainer.data.migration.Migration31to32.Companion.logger
 import co.stone.posmobile.sdk.callback.StoneResultCallback
 import co.stone.posmobile.sdk.merchant.domain.model.Merchant
 import co.stone.posmobile.sdk.merchant.provider.MerchantProvider
 import co.stone.posmobile.sdk.payment.domain.model.CardPaymentMethod
 import co.stone.posmobile.sdk.payment.domain.model.InstallmentTransaction
 import co.stone.posmobile.sdk.payment.domain.model.response.PaymentData
-import co.stone.posmobile.sdk.sendEmail.domain.model.Contact
-import co.stone.posmobile.sdk.sendEmail.domain.model.EmailConfig
-import co.stone.posmobile.sdk.sendEmail.domain.model.EmailReceiptType
-import co.stone.posmobile.sdk.sendEmail.provider.EmailProvider
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlin.coroutines.resumeWithException
 
 class TransactionListViewModel(
-    val transactionProvider: TransactionListProviderWrapper
+    val transactionProvider: TransactionListProviderWrapper,
 ) : ViewModel() {
-
     private val _uiState: MutableStateFlow<TransactionListUiModel> =
         MutableStateFlow(TransactionListUiModel())
     val uiState: StateFlow<TransactionListUiModel> = _uiState.asStateFlow()
@@ -39,42 +30,46 @@ class TransactionListViewModel(
     }
 
     private fun getTransactions() {
-
         viewModelScope.launch {
             _uiState.update { it.copy(loading = true) }
             transactionProvider.getAllTransactions().collect { status ->
 
                 when (status) {
                     is TransactionListProviderWrapper.TransactionListStatus.Success -> {
-                        merchantProvider.getFirstMerchant(object :
-                            StoneResultCallback<Merchant> {
-                            override fun onSuccess(result: Merchant) {
-                                val transactions = status.transactions
-                                    .sortedByDescending { it.transactionId }
-                                    .map { transaction ->
-                                        Transaction(
-                                            id = transaction.transactionId.toString(),
-                                            authorizedAmount = transaction.amountAuthorized.parseCentsToCurrency(),
-                                            authorizationDate = transaction.time,
-                                            atk = transaction.acquirerTransactionKey,
-                                            status = transaction.transactionStatus.name,
-                                            data = transaction,
-                                            merchant = result
+                        merchantProvider.getFirstMerchant(
+                            object :
+                                StoneResultCallback<Merchant> {
+                                override fun onSuccess(result: Merchant) {
+                                    val transactions =
+                                        status.transactions
+                                            .sortedByDescending { it.transactionId }
+                                            .map { transaction ->
+                                                Transaction(
+                                                    id = transaction.transactionId.toString(),
+                                                    authorizedAmount = transaction.amountAuthorized.parseCentsToCurrency(),
+                                                    authorizationDate = transaction.time,
+                                                    atk = transaction.acquirerTransactionKey,
+                                                    status = transaction.transactionStatus.name,
+                                                    data = transaction,
+                                                    merchant = result,
+                                                )
+                                            }
+                                    _uiState.update {
+                                        it.copy(
+                                            transactions = transactions,
+                                            loading = false,
                                         )
                                     }
-                                _uiState.update {
-                                    it.copy(
-                                        transactions = transactions,
-                                        loading = false
-                                    )
                                 }
-                            }
 
-                            override fun onError(stoneStatus: StoneStatus?, throwable: Throwable) {
-                                // Handle error
-                            }
-                        })
-
+                                override fun onError(
+                                    stoneStatus: StoneStatus?,
+                                    throwable: Throwable,
+                                ) {
+                                    // Handle error
+                                }
+                            },
+                        )
                     }
 
                     is TransactionListProviderWrapper.TransactionListStatus.Error -> {
@@ -82,7 +77,7 @@ class TransactionListViewModel(
                             it.copy(
                                 transactions = emptyList(),
                                 loading = false,
-                                errorMessage = status.errorMessage
+                                errorMessage = status.errorMessage,
                             )
                         }
                     }
@@ -99,18 +94,18 @@ class TransactionListViewModel(
                     ?.takeIf { it is CardPaymentMethod.Credit }
                     ?.let { (it as CardPaymentMethod.Credit).installmentTransaction }
                     ?: InstallmentTransaction.None()
-            EmailProvider.create().sendEmail(
-                config = EmailConfig(
-                    Contact("", ""),
-                    listOf(Contact("", "")),
-                    EmailReceiptType.MERCHANT
-                ),
-                data = transaction.data,
-                receiptType = EmailReceiptType.MERCHANT,
-                merchant = transaction.merchant,
-                installmentTransaction = installmentTransaction
-
-            )
+//            EmailProvider.create().sendEmail(
+//                config = EmailConfig(
+//                    Contact("", ""),
+//                    listOf(Contact("", "")),
+//                    EmailReceiptType.MERCHANT
+//                ),
+//                data = transaction.data,
+//                receiptType = EmailReceiptType.MERCHANT,
+//                merchant = transaction.merchant,
+//                installmentTransaction = installmentTransaction
+//
+//            )
         }
     }
 }
@@ -122,11 +117,11 @@ data class Transaction(
     val atk: String?,
     val status: String,
     val data: PaymentData,
-    val merchant: Merchant
+    val merchant: Merchant,
 )
 
 data class TransactionListUiModel(
     val loading: Boolean = false,
     val errorMessage: String? = null,
-    val transactions: List<Transaction> = emptyList()
+    val transactions: List<Transaction> = emptyList(),
 )

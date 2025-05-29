3f8d88dcf00a626564b716546926052d19f843e8
João Carlos Ferreira Marques <joaocarlusferrera@gmail.com>
2025-05-29 17:09:08 -0300

    adjusting theme surface for dark mode;

--- app/src/commonMain/kotlin/br/com/stonesdk/sdkdemo/App.kt
+++ app/src/commonMain/kotlin/br/com/stonesdk/sdkdemo/App.kt
@@ -1,11 +1,14 @@
 package br.com.stonesdk.sdkdemo
 
 import ManageStateScreen
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import br.com.stonesdk.sdkdemo.theme.DemoSdkTheme
 import br.com.stonesdk.sdkdemo.ui.cancel_transactions.CancelScreen
 import br.com.stonesdk.sdkdemo.ui.display.DisplayMessageScreen
 import br.com.stonesdk.sdkdemo.ui.main.MainScreen
@@ -13,26 +16,28 @@ import br.com.stonesdk.sdkdemo.ui.paired_devices.DeviceScreen
 import br.com.stonesdk.sdkdemo.ui.splashscreen.ValidationScreen
 import br.com.stonesdk.sdkdemo.ui.transactions.TransactionScreen
 import br.com.stonesdk.sdkdemo.ui.transactions.transactionList.TransactionListScreen
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-@Preview
 fun DemoApp() {
-    MaterialTheme {
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "splash-screen") {
-            composable("splash-screen") {
-                ValidationScreen(
-                    navController = navController,
-                )
+    DemoSdkTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "splash-screen") {
+                composable("splash-screen") {
+                    ValidationScreen(
+                        navController = navController,
+                    )
+                }
+                composable("home") { MainScreen(navController) }
+                composable("transactions-list") { TransactionListScreen() }
+                composable("cancel-error-transactions") { CancelScreen() }
+                composable("manage-stone-codes") { ManageStateScreen() }
+                composable("paired-devices") { DeviceScreen(navController) }
+                composable("show-message") { DisplayMessageScreen() }
+                composable("make-transaction") { TransactionScreen() }
             }
-            composable("home") { MainScreen(navController) }
-            composable("transactions-list") { TransactionListScreen() }
-            composable("cancel-error-transactions") { CancelScreen() }
-            composable("manage-stone-codes") { ManageStateScreen() }
-            composable("paired-devices") { DeviceScreen(navController) }
-            composable("show-message") { DisplayMessageScreen() }
-            composable("make-transaction") { TransactionScreen() }
         }
     }
 }
